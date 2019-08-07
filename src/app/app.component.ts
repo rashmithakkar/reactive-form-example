7841f8b2b0874070bbd6ac0c622e863b18b71f9b
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-08-07 18:51:57 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-bootstrap';
+  registrationForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.createValidations();
+  }
+  get formControls() {
+    return this.registrationForm.controls;
+  }
+
+  save() {
+    if (this.registrationForm.invalid) {
+      console.log("invalid");
+    } else {
+      console.log("valid");
+
+    }
+  }
+
+  createValidations() {
+    this.registrationForm = this.fb.group({
+      firstName: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      lastName: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      email: ['', [Validators.required, Validators.compose([
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+      ])]],
+      age: ['', Validators.compose([Validators.required, Validators.pattern('(1[89]|[2-4][0-9]|50)')])],
+
+    });
+  }
 }
